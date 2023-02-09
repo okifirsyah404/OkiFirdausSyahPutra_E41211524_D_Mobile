123e7ad4d2169834795aafc8a5564459c0bf3c9a
okifirsyah404 <85375019+okifirsyah404@users.noreply.github.com>
2023-02-09 14:42:04 +0700

    Tugas minggu 1

--- app/src/main/java/com/okifirsyah/okifirdaussyahputra_e41211524_gold_mobile/MainActivity.kt
+++ app/src/main/java/com/okifirsyah/okifirdaussyahputra_e41211524_gold_mobile/MainActivity.kt
@@ -2,10 +2,52 @@ package com.okifirsyah.okifirdaussyahputra_e41211524_gold_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnIncrement: Button
+    lateinit var btnDecrement: Button
+    lateinit var tvResult: TextView
+
+    fun initComponent() {
+        btnIncrement = findViewById(R.id.btnIncrement)
+        btnDecrement = findViewById(R.id.btnDecrement)
+        tvResult = findViewById(R.id.tvResult)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Minggu 1"
+
+        initComponent()
+
+        tvResult.text = "0"
+
+        btnIncrement.setOnClickListener {
+            increment()
+        }
+
+        btnDecrement.setOnClickListener {
+            decrement()
+        }
     }
+
+    fun increment() {
+        var result = tvResult.text.toString().toInt()
+        result++
+        tvResult.text = result.toString()
+    }
+
+    fun decrement() {
+        var result = tvResult.text.toString().toInt()
+
+        if (result > 0) result--
+
+        tvResult.text = result.toString()
+    }
+
 }
