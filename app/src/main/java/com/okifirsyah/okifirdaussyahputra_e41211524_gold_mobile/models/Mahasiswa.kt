722335651a8b306538ad3407f921ffc7591cb281
okifirsyah404 <85375019+okifirsyah404@users.noreply.github.com>
2023-02-28 13:18:55 +0700

    Update minggu 3

--- app/src/main/java/com/okifirsyah/okifirdaussyahputra_e41211524_gold_mobile/models/Mahasiswa.kt
+++ app/src/main/java/com/okifirsyah/okifirdaussyahputra_e41211524_gold_mobile/models/Mahasiswa.kt
@@ -1,4 +1,3 @@
 package com.okifirsyah.okifirdaussyahputra_e41211524_gold_mobile.models
 
-data class Mahasiswa(val name: String, val nim: String, val phoneNumber: String) {
-}
+data class Mahasiswa(val name: String, val nim: String, val phoneNumber: String)
